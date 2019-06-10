e891e7f669389d3d2059b8f9648f39aa41b52e0c
fankaisong <kfan@tamu.edu>
2019-06-10 02:04:54 -0500

    deploy SVM algorithm

---
+++ SVM.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+class SVM:
+    def train(self, train, label, epoch, alpha):
+        feature_size = len(train[0]) 
+        data_size = len(train)
+        lambda_v = 1/epoch
+        
+        self.weight = np.ones(feature_size)
+        
+        
+        for e in range(epoch):
+            
+            sum = 0
+            for x in train:
+                sum += max(0,1-np.dot(x,self.weight))
+            loss = lambda_v*np.linalg.norm(self.weight)**2 + sum
+            
+            print("Epoch {}/{}: Loss is {}".format(e+1,epoch,loss))
+            
+            for i in range(data_size):
+                p = label[i] * np.dot(train[i],self.weight) 
+    
+                if p >= 0: #prediction is correct
+                    self.weight = self.weight - alpha*(2*lambda_v*self.weight)
+                else:
+                    self.weight = self.weight + alpha*(label[i]*train[i] - 2*lambda_v*self.weight)
+                
+    def predict(self,test):
+        
+        res = []
+        
+        for t in test:
+            res.append(np.dot(t,self.weight))
+        
+        return res
+        
---
+++ main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[77]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+from SVM import *   
+                
+        
+
+
+
+df = pd.read_csv('iris.csv')
+
+sepalLength = df['SepalLengthCm'][:100].values
+sepalWidth = df['SepalWidthCm'][:100].values
+species = df['Species'][:100].values
+
+feature = []
+for i in range(100):
+    temp = []
+    temp = [sepalLength[i]] + [sepalWidth[i]]
+    feature.append(temp)
+
+#Iris-setosa is 1   Iris-versicolor is -1
+label = [1]*50 + [-1]*50
+
+
+#shuffle
+feature, label = shuffle(feature,label)
+#split dataset into 90% for training and 10% validation
+feature_train, feature_test, label_train, label_test = train_test_split(feature, label, test_size=0.1)
+
+feature_train = np.array(feature_train)
+label_train = np.array(label_train)
+
+feature_test = np.array(feature_test)
+label_test = np.array(label_test)
+
+
+# In[208]:
+
+
+a = 0.0001
+epoch = 37
+svm = SVM()
+svm.train(feature_train, label_train,epoch, a)
+
+res = svm.predict(feature_test)
+count = 0
+
+for i in range(len(label_test)):
+    if(label_test[i] * res[i] >0):
+        count += 1
+print(count/len(label_test))
+
+
+
+
+
+
